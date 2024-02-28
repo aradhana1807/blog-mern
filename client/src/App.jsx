4f8121b67f1734b056ea963d18e36cf14ee3f4f7
aradhana1807 <aradhana.zero@gmail.com>
2024-02-28 20:13:25 +0530

    added pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
-
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+import Projects from './pages/Projects';
 function App() {
   return (
-    <>
-      <div >
-        App
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
