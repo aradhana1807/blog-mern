09092e02149da8134c6387fabda778a0f6ef92a4
aradhana1807 <aradhana.zero@gmail.com>
2024-02-29 11:10:50 +0530

    create header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,9 +3,13 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      display: ["Poppins", "sans-serif"],
+    },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
