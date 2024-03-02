12f69c7eb89d33d1e8db22f83d7a1d6f07d869ea
aradhana1807 <aradhana.zero@gmail.com>
2024-03-02 16:22:22 +0530

    complete create post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createPost } from '../controllers/post.controller.js';
+import { create } from '../controllers/post.controller.js';
 const router = express.Router();
 
-router.post('/create', verifyToken, createPost);
+router.post('/create', verifyToken, create);
+export default router;
