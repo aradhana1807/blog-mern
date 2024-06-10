ab6e8207462f433b2802ce2ee097976c2f0cba8e
Aradhana Nayak <aradhana.zero@gmail.com>
2024-06-10 15:59:57 +0530

    added ui change

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,41 +1,40 @@
-import { useEffect, useState } from "react"
-import { useLocation } from "react-router-dom"
-import DashSidebar from "../components/DashSidebar"
-import DashProfile from "../components/DashProfile"
-import DashPosts from "../components/DashPosts"
-import DashUsers from "../components/DashUsers"
-import DashComment from "../components/DashComment"
-import DashboardComp from "../components/DashboardComp"
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
+import DashUsers from "../components/DashUsers";
+import DashComment from "../components/DashComment";
+import DashboardComp from "../components/DashboardComp";
 export default function Dashboard() {
-  const location = useLocation()
-  const [tab, setTab] = useState('')
+  const location = useLocation();
+  const [tab, setTab] = useState("");
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const tabFromUrl = urlParams.get('tab')
+    const tabFromUrl = urlParams.get("tab");
 
     if (tabFromUrl) {
       setTab(tabFromUrl);
     }
-  }, [location.search])
-
+  }, [location.search]);
 
   return (
-    <div className="font-display min-h-screen flex flex-col md:flex-row">
+    <div className="font-display min-h-screen flex flex-col md:flex-row dark:bg-[#111111]">
       <div className="md:w-56">
         {/* Sidebar */}
         <DashSidebar />
       </div>
 
       {/* profile... */}
-      {tab === 'profile' && <DashProfile />}
+      {tab === "profile" && <DashProfile />}
       {/* posts */}
-      {tab === 'posts' && <DashPosts />}
+      {tab === "posts" && <DashPosts />}
       {/* users */}
-      {tab === 'users' && <DashUsers />}
+      {tab === "users" && <DashUsers />}
       {/* comments */}
-      {tab === 'comments' && <DashComment />}
+      {tab === "comments" && <DashComment />}
       {/* dashboard component */}
-      {tab === 'dash' && <DashboardComp />}
+      {tab === "dash" && <DashboardComp />}
     </div>
-  )
+  );
 }
